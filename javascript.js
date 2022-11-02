d6d6ee5294d81e6baaf14913f6215f4c28f80f3c
Sean Wilson <Wilsonsa94@gmail.com>
2022-11-02 15:25:14 -0500

    Make game loop five times.

--- javascript.js
+++ javascript.js
@@ -1,10 +1,40 @@
-let opt = ["Rock", "Paper", "Scissors"]
+let opt = ["rock", "paper", "scissors"]
+
+function getUserChoice() {
+    var user_choice = prompt("Rock, Paper, Scissors: ").toLowerCase()
+    console.log(`User: ${user_choice}`)
+    return user_choice
+}
+
 
 function getComputerChoice() {
     let index = Math.floor(Math.random() * 3)
     let com_choice = opt[index]
+    console.log(`Computer: ${com_choice}`)
     return com_choice
 }
 
-let computer = getComputerChoice()
-console.log(computer)
+
+function RPS(computer, user) {
+    if ((computer === "rock" && user === "scissors") ||
+       (computer === "paper" && user === "rock") ||
+       (computer === "scissors" && user === "paper")) {
+        return `Sorry, ${computer} beats ${user}, you lose.`
+    }
+    else if (computer === user) {
+        return `Ohh, you both picked ${user}. It's a tie!`
+    }
+    else {
+        return `Congrats, ${user} beats ${computer}, you win!`
+    }
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let computer = getComputerChoice()
+        let user = getUserChoice()
+        console.log(RPS(computer, user))
+    }
+}
+
+game()
